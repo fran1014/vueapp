9d2c0da491e7028e362f304894b7c1eb30da23ae
Franklin <71532004+fran1014@users.noreply.github.com>
2023-10-04 10:16:38 +0200

    creating counter functionality

---
+++ src/components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>Counter</h2>
+
+    <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+    <button>+1</button>
+    <button>-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 5,
+    };
+  },
+  methods: {
+    getSquareValue() {
+      return this.counter * this.counter;
+    },
+  },
+  computed: {
+    squareCounter() {
+      return this.counter * this.counter;
+    },
+  },
+};
+</script>
+
+<style></style>

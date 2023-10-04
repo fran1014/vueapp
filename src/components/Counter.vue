a12762cc96ec72209cb4eb3c9c25696bbd90fc3d
Franklin <71532004+fran1014@users.noreply.github.com>
2023-10-04 10:27:55 +0200

    creatinbg button functionality

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -3,8 +3,8 @@
     <h2>Counter</h2>
 
     <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
-    <button>+1</button>
-    <button>-1</button>
+    <button v-on:click="increase">+1</button>
+    <button v-on:click="derease">-1</button>
   </div>
 </template>
 
@@ -19,6 +19,12 @@ export default {
     getSquareValue() {
       return this.counter * this.counter;
     },
+    increase() {
+      this.counter++;
+    },
+    derease() {
+      this.counter--;
+    },
   },
   computed: {
     squareCounter() {
@@ -28,4 +34,20 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+button {
+  background-color: #64bb87;
+  border-radius: 5px;
+  border: 1px solid #fff;
+  color: #fff;
+  cursor: pointer;
+  margin: 0 5px;
+  padding: 5px 15px;
+  transition: 0.3s ease-in-out;
+}
+
+button:hover {
+  background-color: #5aa67b;
+  transition: 0.3 ease-in-out;
+}
+</style>
